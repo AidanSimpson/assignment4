68164d88a556305da5b98c57c46c315c52b16cdc
Aidan Simpson <dishmaster96@gmail.com>
2016-10-31 20:14:44 -0500

    Added Files
    HTML CSS and JavaScript template files were added.

---
+++ js/main.js
@@ -0,0 +1,20 @@
+// JavaScript Document
+
+(function() { //Enclosing code in this will immediatly run the code within.
+"use strict"; //This is a way to opt in to a restricted varient of JavaScript. This makes it immposible to create global variables.
+console.log("SEAF Fired"); //Self-Executing Anonymous Function
+
+//Variables
+//var greeting = "Hello";  //string
+//var score = 3; //integer
+//var gameOver = true; //boolean
+//var distance = 43.1; //floating point
+//var dog = null; //null, not type or value has been set.
+
+//Functions
+
+
+//Listeners
+
+
+})();
